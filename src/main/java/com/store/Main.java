baca196213916b975b970ea2f6ac5cd7cb43ba18
kymanzi <victorkimanzi2000@gmail.com>
2026-08-12 14:49:22 +0300

    i have kept my previous code as a comment, and i am running the new code that i created to test the database

--- src/main/java/com/store/Main.java
+++ src/main/java/com/store/Main.java
@@ -1,4 +1,4 @@
-<!-- package com.store;
+/*package com.store;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -23,5 +23,45 @@ public class Main extends Application{
     public static void main(String[] args) {
         launch();
     }
-} -->
+} */
+package com.store;
 
+import com.store.database.Database;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.sql.Connection;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+
+        try (Connection connection = Database.connect()) {
+
+            System.out.println("SQLite connection successful!");
+
+        } catch (Exception e) {
+
+            System.out.println("Database connection failed!");
+            e.printStackTrace();
+        }
+
+        Label label = new Label("Store Inventory System");
+
+        StackPane root = new StackPane(label);
+
+        Scene scene = new Scene(root, 800, 500);
+
+        stage.setTitle("Store Inventory System");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
