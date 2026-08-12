e474d505c85efc8087c85bef1ae20bc1b132f8ad
kymanzi <victorkimanzi2000@gmail.com>
2026-08-12 17:20:58 +0300

    created the login controller for the login page

---
+++ src/main/java/com/store/controller/LoginController.java
@@ -0,0 +1,77 @@
+package com.store.controller;
+
+import com.store.dao.UserDAO;
+import com.store.model.User;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class LoginController {
+
+    private final UserDAO userDAO = new UserDAO;
+
+    public void showLogin(Stage stage) {
+        Label title = new Label("HITECH STORE MANAGEMENT SYSTEM");
+
+        Label usernameLabel = new Label("Password");
+        TextField usernameField = new TextField();
+        usernameField.setPromptText("Enter username");
+
+        Label passwordLabel = new Label("Password");
+        PasswordField passwordField = new PasswordField();
+        passwordField.setPromptText("Enter password");
+
+        Button loginButton = new Button("LOGIN");
+
+        Label messageLabel = new Label();
+
+        loginButton.setOnAction(event -> {
+
+            String username = usernameField.getText().trim();
+            String password = passwordField.getText();
+
+            if (username.isEmpty() || password.isEmpty()) {
+                messageLabel.setText("Please enter usrname and password.");
+                return;
+            }
+
+            User user = userDAO.authenticate(username, password);
+
+            if (user != null) {
+                messageLabel.setText(
+                        "Login successful. Welcome " + user.getUsername()
+                );
+            } else {
+                messageLabel.setText("Invalid username or password.");
+            }
+        });
+
+        VBox layout = new VBox(
+                10,
+                title,
+                usernameLabel,
+                passwordLabel,
+                passwordField,
+                loginButton,
+                messageLabel
+
+        );
+
+        layout.setAlignment(Pos.CENTER);
+        layout.setPadding(new Insets(30));
+
+        Scene scene = new Scene(layout, 500, 450);
+
+        stage.setTitle("Login - HITECH Store Management Ssytem");
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+}
